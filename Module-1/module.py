4faf3b1ba2c3bfa2d13c4aa98162a5d706477191
zalakpatel8486 <122158774+zalakpatel8486@users.noreply.github.com>
2023-01-13 11:03:54 +0530

    Add files via upload

---
+++ Module-1/module.py
@@ -0,0 +1,9 @@
+#check if number is positive ,negative or zero
+num = int(input("enter a number to check it postive/negative or zero:"))
+if (num > 0):
+   print("its a postive number :",num)
+elif (num < 0):
+   print("Its a negative number :",num)
+else:
+   print("Number is ZERO")
+
