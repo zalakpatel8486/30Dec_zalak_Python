7ffa2aad340189d817eb362b75f5d6514f680638
zalakpatel8486 <122158774+zalakpatel8486@users.noreply.github.com>
2023-01-12 21:37:22 -0800

    Delete Module-1 directory

--- Module-1/module.py
+++
@@ -1,9 +0,0 @@
-#check if number is positive ,negative or zero
-num = int(input("enter a number to check it postive/negative or zero:"))
-if (num > 0):
-   print("its a postive number :",num)
-elif (num < 0):
-   print("Its a negative number :",num)
-else:
-   print("Number is ZERO")
-
