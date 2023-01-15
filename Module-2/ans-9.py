2c003c2a5525f291806422813bc581d0129ab6f3
zalakpatel8486 <122158774+zalakpatel8486@users.noreply.github.com>
2023-01-15 15:42:35 +0530

    Add files via upload

---
+++ Module-2/ans-9.py
@@ -0,0 +1,8 @@
+a = int(input("enter first value :"))
+b = int(input("enter second value :"))
+c = int(input("enter third value :"))
+
+if a == b or b == c or c == a :
+    print("sum is :" , 0)
+else :
+    print("sum is :", a+b+c)
